c7fd70e67269863bd1ceaa349dd50359a242a4de
md1300 <md.tawhid.khan1998@gmail.com>
2025-01-07 10:44:29 +0600

    create loading page and error page

--- src/app/(dashboards)/createUser/page.jsx
+++ src/app/(dashboards)/createUser/page.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const createUserPage = () => {
+    console.log(name)
     return (
         <div>
             <h1>this is create user page</h1>
---
+++ src/app/error.jsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react';
+
+const error = () => {
+    return (
+        <div>
+            <h1> app error page .................</h1>
+        </div>
+    );
+};
+
+export default error;
